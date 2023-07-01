bff16527da081c74d8acdffb7680a6a183755861
Ant Weiss <anton@otomato.io>
2023-07-01 17:42:31 +0300

    Setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,10 +28,7 @@ VERSION_STRING=5:24.0.0-1~ubuntu.22.04~jammy
 sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
 
 #install node
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 
-#install yarn
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt install -y yarn
+sudo npm install --global yarn
