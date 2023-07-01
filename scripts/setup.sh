5c61f4c80ce4376a673e4b861c026284aa538c2a
Ant Weiss <anton@otomato.io>
2023-07-01 16:53:45 +0300

    Setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,31 +4,34 @@ sudo apt-get update
 
 #Install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install -y apt-transport-https \
-	                ca-certificates \
-		        curl \
-		        software-properties-common \
+                        ca-certificates \
+                        curl \
+                        software-properties-common \
                         jq \
-			pkg-config \
-			wget \
-			build-essential \
-			make
+                        pkg-config \
+                        wget \
+                        build-essential \
+                        make \
+                        gnupg
 
-#Add Docker’s official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo add-apt-repository \
-	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	      $(lsb_release -cs) \
-	         stable"
-
-sudo apt-get update && sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu
+sudo apt-get update
+VERSION_STRING=5:24.0.0-1~ubuntu.22.04~jammy
+sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
 
 #install node
-sudo snap install node --classic
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
 
 #install yarn
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt install -y yarn
-
