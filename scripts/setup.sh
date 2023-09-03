cf58100bb1c388cb4abd72ec8dfb6e5d39675b65
Ant Weiss <anton@stagecentral.io>
2023-09-03 12:50:26 +0300

    Update install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #! /bin/bash -xf
 
+# Cancel needrestart popups
+sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+
+# Initial apt update
 sudo apt-get update
 
 #Install packages to allow apt to use a repository over HTTPS:
@@ -40,7 +44,10 @@ VERSION_STRING=5:24.0.0-1~ubuntu.22.04~jammy
 sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
 
 #install node
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
 sudo apt-get install -y nodejs
 
 sudo npm install --global yarn
